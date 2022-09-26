44dd6c3955404eb0e1259ea253d2af056fbb7e12
dddraxxx <15207615513@qq.com>
2022-09-26 10:56:29 +0000

    minor change for breast data and save pred result

--- eval.py
+++ eval.py
@@ -117,8 +117,6 @@ def main():
             print("Validation subset {}".format(val_subset))
             gen = ds.generator(val_subset, loop=False)
             results = framework.validate(sess, gen, keys=keys, summary=False, show_tqdm=True, predict=True)
-            # save results as pickle
-            print('finish saving pkl')
             for i in range(len(results['jaccs'])):
                 print(results['id1'][i], results['id2'][i], np.mean(results['dices'][i]), np.mean(results['jaccs'][i]), np.mean(
                     results['landmark_dists'][i]), *results['dices'][i], file=fo)
@@ -140,6 +138,7 @@ def main():
 
         with open(path_prefix + '.pkl', 'wb') as f:
             pickle.dump(results, f)
+        print('finish saving pkl')
             
 def save_npy_results(results, path):
     keys = ['image_fixed', 'warped_moving', 'warped_seg_moving']
